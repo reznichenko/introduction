017a802144e8edbe562494e94f7d114eac5dd0cf
reznichenko <bogdan.r@inbox.ru>
2020-12-30 13:28:18 +0300

    HW6

---
+++ HW6/HW6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Task Manager";
+            pringProcess();
+            Console.WriteLine("Введите ID жертвы");
+            if(!int.TryParse(Console.ReadLine(), out int idProcess)){
+                Console.WriteLine("Введите число.");
+            }
+            else
+            {
+                Process victim = Process.GetProcessById(idProcess);
+                Console.WriteLine($"Убиваю {victim.ProcessName}");
+                try
+                {
+                    victim.Kill();
+                }
+                catch(Win32Exception)
+                {
+                    Console.WriteLine("И кто тебе дал право?");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        static void pringProcess() {
+            Process[] prc = Process.GetProcesses();
+
+            foreach (var item in prc)
+            {
+                Console.WriteLine($"ID = {item.Id} Name = {item.ProcessName}");
+            }
+            Console.WriteLine($"\nВсего процессов {prc.Length + 1}");
+        }
+    }
+}
